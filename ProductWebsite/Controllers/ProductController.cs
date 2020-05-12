ada1d8d4a4d58daa22afe8f10237761187fe3263
Sumit Tuladhar <soomeet@hotmail.com>
2020-05-12 12:25:39 -0700

    first commit

---
+++ ProductWebsite/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebsite.Models;
+using ProductWebsite.Services;
+
+namespace ProductWebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly JsonFileProductService ProductService;
+        public ProductController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> Products = ProductService.GetProducts();
+            return View("Product",Products);
+        }
+    }
+}
